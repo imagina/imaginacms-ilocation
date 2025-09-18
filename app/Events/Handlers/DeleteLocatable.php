bb35eb3552919071e115f91d112b2dca796f9e92
Cristian Marin <marin101998@gmail.com>
2025-09-18 10:28:21 -0500

    + fix | v12.x 18-09-25 | Fix Locatable  relations change name relation

--- app/Events/Handlers/DeleteLocatable.php
+++ app/Events/Handlers/DeleteLocatable.php
@@ -3,17 +3,16 @@
 namespace Modules\Ilocation\Events\Handlers;
 
 
-
 class DeleteLocatable
 {
 
-    public function handle($event): void
-    {
+  public function handle($event): void
+  {
 
-        $params = $event->params;
-        $model = $params['model'];
+    $params = $event->params;
+    $model = $params['model'];
 
-        //Delete Locations
-        $model->locations()->detach();
-    }
+    //Delete Locations
+    $model->locatable()->delete();
+  }
 }
--- app/Events/Handlers/StoreLocatable.php
+++ app/Events/Handlers/StoreLocatable.php
@@ -4,42 +4,44 @@ namespace Modules\Ilocation\Events\Handlers;
 
 class StoreLocatable
 {
-    public function handle($event): void
-    {
-        $params = $event->params;
+  public function handle($event): void
+  {
+    $params = $event->params;
 
-        // Get specific data
-        $dataFromRequest = $params['data'];
-        $model = $params['model'];
+    // Get specific data
+    $dataFromRequest = $params['data'];
+    $model = $params['model'];
 
-        // Handle Form
-        if (!empty($dataFromRequest['locatable'])) {
-            $this->syncExtraFillable($dataFromRequest, $model);
-        }
+    // Handle Form
+    if (!empty($dataFromRequest['locatable'])) {
+      $this->syncExtraFillable($dataFromRequest, $model);
     }
+  }
 
-    public function syncExtraFillable($params, $model): void
-    {
-        $cityId = $params['locatable']['city_id'] ?? null;
-        $countryId = $params['locatable']['country_id'] ?? null;
-        $provinceId = $params['locatable']['province_id'] ?? null;
-        $address = $params['locatable']['address'] ?? null;
-        $lat = $params['locatable']['latitude'] ?? null;
-        $lng = $params['locatable']['longitude'] ?? null;
+  public function syncExtraFillable($params, $model): void
+  {
+    $cityId = $params['locatable']['city_id'] ?? null;
+    $countryId = $params['locatable']['country_id'] ?? null;
+    $provinceId = $params['locatable']['province_id'] ?? null;
+    $address = $params['locatable']['address'] ?? null;
+    $lat = $params['locatable']['latitude'] ?? null;
+    $lng = $params['locatable']['longitude'] ?? null;
 
-        if ($cityId || $countryId || $provinceId || $lat || $lng || $address) {
-            $locatableRepository = app('Modules\Ilocation\Repositories\LocatableRepository');
-            $locatableRepository->updateOrCreate([
-                'entity_type' => get_class($model),
-                'entity_id' => $model->id,
-            ], [
-                'city_id' => $cityId,
-                'country_id' => $countryId,
-                'province_id' => $provinceId,
-                'address' => $address,
-                'latitude' => $lat,
-                'longitude' => $lng,
-            ]);
-        }
+    if ($cityId || $countryId || $provinceId || $lat || $lng || $address) {
+      $locatableRepository = app('Modules\Ilocation\Repositories\LocatableRepository');
+      $systemName = strtolower(str_replace('\\', '_', get_class($model))) . '_' . $model->id;
+      $locatableRepository->updateOrCreate([
+        'systemName' => $systemName,
+        'entityType' => get_class($model),
+        'entityId' => $model->id,
+      ], [
+        'city_id' => $cityId,
+        'country_id' => $countryId,
+        'province_id' => $provinceId,
+        'address' => $address,
+        'latitude' => $lat,
+        'longitude' => $lng,
+      ]);
     }
+  }
 }
